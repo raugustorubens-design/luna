209aea413232efe4a0279be9d67a5c95cccde819
raugustorubens-design <raugustorubens@gmail.com>
2026-03-23 05:29:23 -0300

    Change app.listen to bind to 0.0.0.0

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// 🔥 AQUI É A ÚNICA MUDANÇA
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 rodando na porta " + PORT);
 });
